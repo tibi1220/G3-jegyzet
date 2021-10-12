c24d91297fe8877b09ea7318fd6b7e6ba61f41cc
Tibor Sandor <tiborsandor2001@gmail.com>
2021-10-12 11:24:00 +0200

    Add cleanup script

---
+++ cleanup.sh
@@ -0,0 +1,6 @@
+find . -name '*.log' -delete;
+find . -name '*.aux' -delete;
+find . -name '*.fdb_latexmk' -delete;
+find . -name '*.fls' -delete;
+find . -name '*.synctex.gz' -delete;
+find . -name '*.pdf' -delete;
