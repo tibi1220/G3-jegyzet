09ce354cc926f1e63fb1e24dcd2021f6003f1371
Tibor Sandor <tiborsandor2001@gmail.com>
2021-10-19 23:39:13 +0200

    Update config files

--- cleanup.sh
+++ cleanup.sh
@@ -4,3 +4,5 @@ find . -name '*.fdb_latexmk' -delete;
 find . -name '*.fls' -delete;
 find . -name '*.synctex.gz' -delete;
 find . -name '*.pdf' -delete;
+find . -name '*.xdv' -delete;
+find . -name '*.toc' -delete;
