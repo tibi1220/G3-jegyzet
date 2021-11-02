0aec6cbf55674e73641a10f4098a2c0bf964f372
Tibor Sandor <tiborsandor2001@gmail.com>
2021-11-02 11:06:12 +0100

    Update cleanup script 🧹

--- cleanup.sh
+++ cleanup.sh
@@ -3,6 +3,6 @@ find . -name '*.aux' -delete;
 find . -name '*.fdb_latexmk' -delete;
 find . -name '*.fls' -delete;
 find . -name '*.synctex.gz' -delete;
-find . -name '*.pdf' -delete;
+find . -name '*.out' -delete;
 find . -name '*.xdv' -delete;
 find . -name '*.toc' -delete;
